c1285b49b4effc6395913b650217824376ae2fa1
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-10 08:24:21 +0000

    [Finish] 3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,10 +21,16 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
   
-   def update
+  def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)  
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
   
    private
