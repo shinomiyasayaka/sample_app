5e87c9922840464b02a83d4e90203551704e2172
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-10 05:38:41 +0000

    [Finish] 3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'lists/new'
   get 'lists' => 'lists#index'
-  get 'lists/show'
+  get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/edit'
-  post 'lists' => 'lists#create'
   get '/top' => 'homes#top'
+  post 'lists' => 'lists#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
