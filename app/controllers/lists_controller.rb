1b7a2227b1e3edfaae84399a011c5c5a897678a3
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-05-07 13:11:42 +0000

    [Finish]3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,10 +4,10 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(list_params)
-    if @list.save
+    @list = List.new(list_params) # １.&2. データを受け取り新規登録するためのインスタンス作成
+    if @list.save # 3. データをデータベースに保存するためのsaveメソッド実行
       flash[:notice] = "投稿に成功しました。"
-      redirect_to list_path(@list.id)
+      redirect_to list_path(@list.id) # 4. show画面へリダイレクト
     else
       flash.now[:alert] = "投稿に失敗しました。"
       redirect_to new_list_path
@@ -43,3 +43,8 @@ class ListsController < ApplicationController
     params.require(:list).permit(:title, :body, :image)
   end
 end
+
+#private 一種の境界線で、「ここから下はこのcontrollerの中でしか呼び出せません」という意味
+#params formから送られてくるデータはparamsの中に入っています。
+#require 送られてきたデータの中からモデル名(ここでは:list)を指定し、データを絞り込みます。
+#permit requireで絞り込んだデータの中から、保存を許可するカラムを指定します。
